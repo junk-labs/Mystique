9849aa4941d880f85914bd802645ed15c620b866
karno <webmaster@starwing.net>
2011-09-03 15:15:01 +0900

    SookIkemen

--- SookIkemen/EntryPoint.cs
+++ SookIkemen/EntryPoint.cs
@@ -1,7 +1,12 @@
-﻿using System.ComponentModel.Composition;
+﻿using System;
+using System.ComponentModel.Composition;
 using System.Linq;
 using Acuerdo.Plugin;
+using Inscribe.Common;
 using Inscribe.Communication.Posting;
+using Inscribe.Core;
+using Inscribe.Storage;
+using Inscribe.Subsystems;
 
 namespace SookIkemen
 {
@@ -15,12 +20,28 @@ namespace SookIkemen
 
         public double Version
         {
-            get { return 0; }
+            get { return 1.1; }
         }
 
         public void Loaded()
         {
-            Inscribe.Core.Initializer.OnStandbyApp += () => Inscribe.Storage.AccountStorage.Accounts.ForEach(a => PostOffice.UpdateTweet(a, "スークイケメンﾅｰｰｰｰｰｰｰｰｰｰｰｰｰｰｰｰｰｰｰｰｰｰｰｰｰｰｰｰｰｰｰｰ #sookikemen"));
+            KeyAssignCore.RegisterOperation("SookIkemen", () =>
+                KeyAssignHelper.ExecuteTabAction(tab =>
+                {
+                    try
+                    {
+                        tab.TabProperty.LinkAccountInfos.ForEach(a => PostOffice.UpdateTweet(a, "スークイケメンﾅｰｰｰｰｰｰｰｰｰｰｰｰｰｰｰｰｰｰｰｰｰｰｰｰｰｰｰｰｰｰｰｰ #sookikemen"));
+                    }
+                    catch (Exception e)
+                    {
+                        ExceptionStorage.Register(e, ExceptionCategory.PluginError, "スークイケメンﾅｰｰｰｰｰｰｰｰｰｰｰｰｰｰに失敗しました: " + e.Message, () => Loaded());
+                    }
+                }));
+            KeyAssignCore.RegisterOperation("HageSon", () =>
+                KeyAssignHelper.ExecuteTVMAction(tvm =>
+                    {
+                        KernelService.MainWindowViewModel.InputBlockViewModel.SetOpenText(true, true);
+                    }));
         }
 
         public IConfigurator ConfigurationInterface
