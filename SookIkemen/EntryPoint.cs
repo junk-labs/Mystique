e0299f598351bb6ae938f8dcc8c6bda1de658abf
karno <webmaster@starwing.net>
2011-09-04 19:02:18 +0900

    refresh S.I. plugin

--- SookIkemen/EntryPoint.cs
+++ SookIkemen/EntryPoint.cs
@@ -2,6 +2,7 @@
 using System.ComponentModel.Composition;
 using System.Linq;
 using Acuerdo.Plugin;
+using Dulcet.Twitter;
 using Inscribe.Common;
 using Inscribe.Communication.Posting;
 using Inscribe.Core;
@@ -30,18 +31,49 @@ namespace SookIkemen
                 {
                     try
                     {
-                        tab.TabProperty.LinkAccountInfos.ForEach(a => PostOffice.UpdateTweet(a, "スークイケメンﾅｰｰｰｰｰｰｰｰｰｰｰｰｰｰｰｰｰｰｰｰｰｰｰｰｰｰｰｰｰｰｰｰ #sookikemen"));
+                        tab.TabProperty.LinkAccountInfos.ForEach(a =>
+                            PostOffice.UpdateTweet(a, "スークイケメンﾅｰｰｰｰｰｰｰｰｰｰｰｰｰｰｰｰｰｰｰｰｰｰｰｰｰｰｰｰｰｰｰｰ #sook_ikemen"));
                     }
                     catch (Exception e)
                     {
-                        ExceptionStorage.Register(e, ExceptionCategory.PluginError, "スークイケメンﾅｰｰｰｰｰｰｰｰｰｰｰｰｰｰに失敗しました: " + e.Message, () => Loaded());
+                        ExceptionStorage.Register(e, ExceptionCategory.PluginError, "スークイケメンﾅｰｰｰｰｰｰｰｰｰｰｰｰｰｰに失敗しました: " + e.Message);
                     }
                 }));
-            KeyAssignCore.RegisterOperation("HageSon", () =>
+            KeyAssignCore.RegisterOperation("SenselessRetweet", () =>
                 KeyAssignHelper.ExecuteTVMAction(tvm =>
                     {
+                        var ts = tvm.Tweet.Status as TwitterStatus;
+                        if (ts == null) return;
                         KernelService.MainWindowViewModel.InputBlockViewModel.SetOpenText(true, true);
+                        KernelService.MainWindowViewModel.InputBlockViewModel.SetText(BuildSenseless(ts));
                     }));
+            KeyAssignCore.RegisterOperation("SenselessRetweetFast", () =>
+                KeyAssignHelper.ExecuteTVMAction(tvm =>
+                {
+                    try
+                    {
+                        var ts = tvm.Tweet.Status as TwitterStatus;
+                        if (ts == null) return;
+                        tvm.Parent.TabProperty.LinkAccountInfos.ForEach(
+                            ai => PostOffice.UpdateTweet(ai, BuildSenseless(ts)));
+                    }
+                    catch (Exception e)
+                    {
+                        ExceptionStorage.Register(e, ExceptionCategory.PluginError, "非常識RTに失敗しました: " + e.Message);
+                    }
+                }));
+        }
+
+        private string BuildSenseless(TwitterStatus ts)
+        {
+            if (ts.RetweetedOriginal != null)
+            {
+                return "… RT @" + ts.RetweetedOriginal.User.ScreenName + ": " + ts.RetweetedOriginal.Text;
+            }
+            else
+            {
+                return "… RT @" + ts.User.ScreenName + ": " + ts.Text;
+            }
         }
 
         public IConfigurator ConfigurationInterface
