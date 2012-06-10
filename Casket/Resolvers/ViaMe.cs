b18a2b7074420baedd7db5987f88d6cc3746851f
karno <webmaster@starwing.net>
2012-06-11 02:46:15 +0900

    add via.me resolver

---
+++ Casket/Resolvers/ViaMe.cs
@@ -0,0 +1,17 @@
+﻿using Acuerdo.External.Uploader;
+
+namespace Casket.Resolvers
+{
+    public class ViaMe : IResolver
+    {
+        public bool IsResolvable(string url)
+        {
+            return url.StartsWith("http://via.me/");
+        }
+
+        public string Resolve(string url)
+        {
+            return url + "/thumb/r600x600";
+        }
+    }
+}
