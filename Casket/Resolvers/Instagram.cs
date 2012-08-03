be8647bdd0f71f0b1e7c053e6f31d746b9e5673b
yut831 <airman515@gmail.com>
2012-08-03 15:29:06 +0900

    add Instagram resolver

---
+++ Casket/Resolvers/Instagram.cs
@@ -0,0 +1,20 @@
+﻿using Acuerdo.External.Uploader;
+
+namespace Casket.Resolvers
+{
+    public class Instagram : IResolver
+    {
+        public bool IsResolvable(string url)
+        {
+            return url.StartsWith("http://instagram.com/") || url.StartsWith("http://instagr.am/");
+        }
+
+        public string Resolve(string url)
+        {
+            if (IsResolvable(url))
+                return url + "media/?size=l";
+            else
+                return null;
+        }
+    }
+}
