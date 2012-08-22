235974d79c7b671ecaabf84c1f1afbefd0dcccbe
yut831 <airman515@gmail.com>
2012-08-22 16:40:06 +0900

    add Photozou resolver

---
+++ Casket/Resolvers/Photozou.cs
@@ -0,0 +1,20 @@
+﻿using Acuerdo.External.Uploader;
+
+namespace Casket.Resolvers
+{
+    public class Photozou : IResolver
+    {
+        public bool IsResolvable(string url)
+        {
+            return url.StartsWith("http://photozou.jp/");
+        }
+
+        public string Resolve(string url)
+        {
+            if (IsResolvable(url))
+                return "http://photozou.jp/bin/photo/" + url.Split('/')[6] + "/org.bin";
+            else
+                return null;
+        }
+    }
+}
