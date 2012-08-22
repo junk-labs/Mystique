0d03430f72bb6d8e934b0df8795d49c51301262c
yut831 <airman515@gmail.com>
2012-08-22 16:40:54 +0900

    add Droplr resolver

---
+++ Casket/Resolvers/Droplr.cs
@@ -0,0 +1,20 @@
+﻿using Acuerdo.External.Uploader;
+
+namespace Casket.Resolvers
+{
+    public class Droplr : IResolver
+    {
+        public bool IsResolvable(string url)
+        {
+            return url.StartsWith("http://d.pr/i/");
+        }
+
+        public string Resolve(string url)
+        {
+            if (IsResolvable(url))
+                return url.EndsWith("+") ? url : url + "+";
+            else
+                return null;
+        }
+    }
+}
