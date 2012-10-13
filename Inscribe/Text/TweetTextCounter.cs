38e539c926fd4eb236602435af24d529c064d5cb
ktoku89 <kazuya.tokunaga@gmail.com>
2012-10-14 01:14:42 +0900

    Fix a bug of wrong counting characters with HTTPS URL
    refs karno/Mystique#38

--- Inscribe/Text/TweetTextCounter.cs
+++ Inscribe/Text/TweetTextCounter.cs
@@ -7,17 +7,21 @@ namespace Inscribe.Text
     {
         public static int Count(string input)
         {
-            // URL is MAX 19 Chars.
+            // URL is MAX 20 Chars (if URL has HTTPS scheme, URL is MAX 21 Chars)
             int prevIndex = 0;
             int totalCount = 0;
             foreach (var m in RegularExpressions.UrlRegex.Matches(input).OfType<Match>())
             {
                 totalCount += m.Index - prevIndex;
                 prevIndex = m.Index + m.Groups[0].Value.Length;
-                if (m.Groups[0].Value.Length < TwitterDefine.UrlMaxLength)
+
+                bool isHasHttpsScheme = m.Groups[0].Value.Contains("https");
+
+
+                if (m.Groups[0].Value.Length < TwitterDefine.UrlMaxLength + ((isHasHttpsScheme) ? 1 : 0))
                     totalCount += m.Groups[0].Value.Length;
                 else
-                    totalCount += TwitterDefine.UrlMaxLength;
+                    totalCount += TwitterDefine.UrlMaxLength + ((isHasHttpsScheme) ? 1 : 0);
             }
             totalCount += input.Length - prevIndex;
             return totalCount;
