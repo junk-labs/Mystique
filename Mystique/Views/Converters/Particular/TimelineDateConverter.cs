20a68da5f5298f78de29e9dba563d8caa0e86b2e
fin_alice <ender_alice@yahoo.com>
2014-05-27 10:03:59 +0900

    タイムライン上の日付表記を変更 - 日付が今日でない場合は日付を併記

---
+++ Mystique/Views/Converters/Particular/TimelineDateConverter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+using Inscribe.ViewModels.PartBlocks.MainBlock.TimelineChild;
+
+namespace Mystique.Views.Converters.Particular
+{
+    public class TimelineDateConverter : OneWayConverter<DateTime, string>
+    {
+        public override string ToTarget(DateTime input, object parameter)
+        {
+            var today = DateTime.Now.Date;
+            var tweetDate = input.Date;
+
+            return String.Format((tweetDate == today) ? "{0:D2}:{1:D2}:{2:D2}" : "{3:D2}/{4:D2}/{5:D2}\n{0:D2}:{1:D2}:{2:D2}", input.Hour, input.Minute, input.Second, input.Year % 100, input.Month, input.Day);
+        }
+    }
+}
